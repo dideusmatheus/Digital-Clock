a8fa37c5861beee321f53f254cafab3ac7cbc569
Matheus de Deus <matheus_deusr@hotmail.com>
2022-09-12 18:25:08 -0300

    Colocando cor no circulo dos numeros

--- ts/main.js
+++ ts/main.js
@@ -1,34 +1,45 @@
-let hours = document.getElementById('hours');
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
-let ampm = document.getElementById('ampm');
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+let ampm = document.getElementById("ampm");
 
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSeconds();
+setInterval(() => {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let am = checkAM_PM(h);
 
-//convert 24hr click to 12h clock
-convertAM_PM();
+  //convert 24hr click to 12h clock
+  convertAM_PM();
 
-//add 0 before single digit number 
-h = add0(h);
-m = add0(m);
-s = add0(s);
+  //add 0 before single digit number
+  h = add0(h);
+  m = add0(m);
+  s = add0(s);
 
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
-ampm.innerHTML = am;
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  ampm.innerHTML = am;
 
-function convertAM_PM(){
-    if(h > 12){
-        h = h - 12;
+  function convertAM_PM() {
+    if (h > 12) {
+      h = h - 12;
     }
-}
+  }
 
-function add0(num){
-    if(num < 10){
-        return "0" + num;
+  function add0(num) {
+    if (num < 10) {
+      return "0" + num;
     }
     return num;
-}
+  }
+
+  function checkAM_PM(h){
+    if( h >= 12){
+        return "PM";
+    }
+    return "AM";
+  }
+
+});
